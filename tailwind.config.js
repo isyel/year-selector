daef9661bf274f4862394d725b9b8b0d56c7ee61
isyel <isykaal4saviour@gmail.com>
2024-12-12 19:01:09 +0100

    remove old css, and extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#F0F0F0",
+        accent: {
+          1: "#DE0D6F",
+          2: "#F8CDD9",
+          3: "#731054",
+          contrast: "#FFFFFF",
+        },
+        purple: {
+          1: "#E2F3FC",
+          2: "#EFEAFD",
+          3: "#D6E4F4",
+        },
+        warning: {
+          1: "#FDF4C8",
+          2: "#FFEAD5",
+        },
+        danger: "#FF0000",
+        success: "#D5F6DE",
+      },
+    },
   },
   plugins: [],
 };
