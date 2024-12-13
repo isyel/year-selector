6e801d1afa888661ace61693532aa0ed1c4a434f
isyel <isykaal4saviour@gmail.com>
2024-12-13 18:05:31 +0100

    update configuration for package

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      formats: ["es"],
+      name: "index",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react-router"],
+      input: ["lib/index.ts", "lib/assets/index.css"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
   },
 });
