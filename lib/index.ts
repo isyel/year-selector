bf59722ac1fb074cc84e04df26bd8d458f3452a2
isyel <isykaal4saviour@gmail.com>
2024-12-13 10:35:07 +0100

    add lib exports

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+/* Components */
+export { default as Button } from "./components/Button";
+export { default as Label } from "./components/Label";
+export { default as Text } from "./components/Text";
+export { default as YearSelector } from "./components/YearSelector";
+
+/* Enums */
+export { ButtonVariant } from "./components/Button/enums";
+export { Color } from "./enums/color.enum";
+export { Size } from "./enums/size.enum";
+export { TextWeight } from "./components/Text/enums";
